4e018856dd3f9b4fc792b3b4250990bb47c7dcf6
Amita.Nair <amita.nair@randstadrisesmart.com>
2021-10-11 20:40:21 +0530

    added countries and taglib handler

---
+++ src/main/java/com/randstad/dto/CountryDto.java
@@ -0,0 +1,22 @@
+package com.randstad.dto;
+
+public class CountryDto {
+    private int id;
+    private String countryName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
